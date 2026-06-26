d4b23db5ad09535c36cf0518869711346ada2929
Ronan Corr <ronan_corr@hotmail.com>
2026-06-26 13:41:39 -0400

    create and include layouts.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
 import './styles/globals.css';
+import './styles/layouts.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
