46da4ff575c71595f4571dbd514d6b05dca54e62
Ronan Corr <ronan_corr@hotmail.com>
2026-07-23 07:40:26 -0400

    feat(dropdown): support multi-level nested submenus

--- src/examples/Dropdown/DropdownExamples.tsx
+++ src/examples/Dropdown/DropdownExamples.tsx
@@ -11,20 +11,20 @@ export function DropdownExamples() {
       <h2>Dropdowns</h2>
 
       <div className="row">
-        <DropdownBasic trigger={<Button>Open Basic Menu</Button>}>
+        <DropdownBasic trigger={<Button>Basic Menu</Button>}>
           <button>Profile</button>
           <button>Settings</button>
           <button>Logout</button>
         </DropdownBasic>
 
-        <DropdownAccessible trigger={<Button>Open Accessible Menu</Button>}>
+        <DropdownAccessible trigger={<Button>Accessible Menu</Button>}>
           <DropdownItem>Profile</DropdownItem>
           <DropdownItem>Settings</DropdownItem>
           <DropdownItem>Logout</DropdownItem>
         </DropdownAccessible>
 
         <DropdownAccessible
-          trigger={<Button>Open Accessible Menu with Submenus</Button>}
+          trigger={<Button>Accessible Menu with 3-level Submenus</Button>}
         >
           <DropdownItem>New</DropdownItem>
 
@@ -50,6 +50,31 @@ export function DropdownExamples() {
           <DropdownItem disabled>Rename (disabled)</DropdownItem>
 
           <DropdownItem>Save</DropdownItem>
+        </DropdownAccessible>
+      </div>
+      <div className="row">
+        <DropdownAccessible
+          trigger={<Button>Accessible Menu with 4-level Submenus</Button>}
+        >
+          <DropdownItem>New</DropdownItem>
+
+          <DropdownSubmenu label="Open">
+            <DropdownItem>Project</DropdownItem>
+
+            <DropdownSubmenu label="Recent">
+              <DropdownItem>A</DropdownItem>
+
+              <DropdownSubmenu label="2025">
+                <DropdownItem>January</DropdownItem>
+                <DropdownItem>February</DropdownItem>
+
+                <DropdownSubmenu label="March">
+                  <DropdownItem>Week 1</DropdownItem>
+                  <DropdownItem>Week 2</DropdownItem>
+                </DropdownSubmenu>
+              </DropdownSubmenu>
+            </DropdownSubmenu>
+          </DropdownSubmenu>
         </DropdownAccessible>
       </div>
     </div>
