fc0a268c798922c966dbaf2afeb68e16f6dab05d
Ronan Corr <ronan_corr@hotmail.com>
2026-08-19 08:16:29 -0400

    refactor: remove redundant component links from home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,5 @@
-import { Link } from 'react-router';
-
 export function HomePage() {
   return (
-    <>
-      <p>Explore the accessible components and UI patterns in this project.</p>
-
-      <h2>Components</h2>
-
-      <ul>
-        <li>
-          <Link to="/button">Button</Link>
-        </li>
-        <li>
-          <Link to="/modal">Modal Dialog</Link>
-        </li>
-        <li>
-          <Link to="/dropdown">Dropdown</Link>
-        </li>
-        <li>
-          <Link to="/navigation">Navigation</Link>
-        </li>
-      </ul>
-    </>
+    <p>Explore the accessible components and UI patterns in this project.</p>
   );
 }
